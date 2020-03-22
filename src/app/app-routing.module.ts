5a0f790b0de4d6e80812d197458cc05f0c75c6ae
Sabina Michalczewska <sabina.michalczewska@gmail.com>
2020-03-22 14:21:01 +0100

    Cook module added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CookMainComponent } from './modules/cook/cook-main/cook-main.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/modules/cook/cook-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CookMainComponent } from './cook-main/cook-main.component';
+
+
+const routes: Routes = [{path: 'cook', component: CookMainComponent}];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class CookRoutingModule { }
