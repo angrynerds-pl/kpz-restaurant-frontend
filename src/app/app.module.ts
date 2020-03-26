7ff5ac49679f2e555085db9bdbcd349819a72a9b
KarolJaskolka <jaskolka.karol@gmail.com>
2020-03-26 18:08:42 +0100

    change cook-routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { CookModule } from './modules/cook/cook.module';
-import { CookRoutingModule } from './modules/cook/cook-routing.module';
-
 import { HomeModule } from './modules/home/home.module';
-import { HomeRoutingModule } from './modules/home/home-routing.module';
-
 import { ManagerModule } from './modules/manager/manager.module';
-import { ManagerRoutingModule } from './modules/manager/manager-routing.module';
-
 import { WaiterModule } from './modules/waiter/waiter.module';
-import { WaiterRoutingModule } from './modules/waiter/waiter-routing.module';
 
 @NgModule({
   declarations: [
@@ -24,14 +17,9 @@ import { WaiterRoutingModule } from './modules/waiter/waiter-routing.module';
     BrowserModule,
     AppRoutingModule,
     CookModule,
-    CookRoutingModule,
     HomeModule,
     ManagerModule,
-    WaiterModule,
-    HomeRoutingModule,
-    ManagerRoutingModule,
-    WaiterRoutingModule
-
+    WaiterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
