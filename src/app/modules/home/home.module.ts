82bd8d57e1a4facc21d028543236c5af2327f4ca
mateuszKrawczak <241318@student.pwr.edu.pl>
2020-03-26 17:53:05 +0100

    Added login component in home module

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
