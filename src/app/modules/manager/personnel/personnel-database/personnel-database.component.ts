2d5d1474a05e4274e3c2160ed710362ff9dec9a9
mateuszKrawczak <241318@student.pwr.edu.pl>
2020-04-02 16:20:49 +0200

    added accounts's registration component

--- src/app/modules/manager/personnel/personnel-database/personnel-database.component.ts
+++ src/app/modules/manager/personnel/personnel-database/personnel-database.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
 import { UserService } from 'src/app/services/user.service';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-personnel-database',
   templateUrl: './personnel-database.component.html',
-  styleUrls: ['./personnel-database.component.scss']
+  styleUrls: ['./personnel-database.component.scss'],
+ 
 })
 export class PersonnelDatabaseComponent implements OnInit {
 
-
-  users:User[];
+ 
+ 
+ 
+ users:User[];
+ 
   constructor(private userService:UserService) { }
 
   ngOnInit(): void {
@@ -19,6 +24,7 @@ export class PersonnelDatabaseComponent implements OnInit {
   loadUsers() {
     this.userService.getUsers().subscribe(users => {
       this.users = users;
+      
     });
   }
 }
--- src/app/modules/manager/personnel/personnel-registration/personnel-registration.component.ts
+++ src/app/modules/manager/personnel/personnel-registration/personnel-registration.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/models/user';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import{ Observable } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 @Component({
   selector: 'app-personnel-registration',
   templateUrl: './personnel-registration.component.html',
@@ -7,9 +13,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonnelRegistrationComponent implements OnInit {
 
-  constructor() { }
+  acceptIcon = faCheck;
+  cancelIcon = faTimes;
+
+  newUser: User;
+
+  private userID:number;
+
+  public userForm:FormGroup;
+
+  constructor(private userService:UserService, private toastrService:ToastrService, private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.updateForm();
   }
 
+  updateForm(){
+    this.userForm = this.fb.group({
+      firstName: [null, Validators.required],
+      lastName:[null, Validators.required],
+      username:[null, Validators.required],
+      password:[null, Validators.required],
+      position:[null, Validators.required],
+    });
+  }
+
+  resetForm(){
+    this.userForm.reset();
+  }
+
+  createAccount(){
+    if (this.userForm.valid) {
+      this.userID = this.userService.getLastUserID();
+      this.newUser = Object.assign({}, this.userForm.value);
+      this.newUser.id = this.userID;
+
+      this.userService.addUser(this.newUser);
+
+      this.resetForm();
+      this.toastrService.success("New user added!");
+    }
+  }
 }
