0ddf2a1e2ea93fa9cdedfed54dcb9892f94b8755
KarolJaskolka <jaskolka.karol@gmail.com>
2020-04-14 13:39:24 +0200

    Update room-editor-main & table-option

--- src/app/modules/manager/room-editor/room-editor-main/room-editor-main.component.ts
+++ src/app/modules/manager/room-editor/room-editor-main/room-editor-main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Table } from 'src/app/models/table';
 
 @Component({
   selector: 'app-room-editor-main',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoomEditorMainComponent implements OnInit {
 
+  rows:number;
+  columns: number;
+
+  rowsOption: Array<number>;
+  columnsOption: Array<number>;
+
+  grid: Array<Array<Table>>;
+
+  tables: Array<Table> = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.rowsOption = Array.from(Array(8).keys()).map(x => x + 3);
+    this.columnsOption = Array.from(Array(3).keys()).map(x => x + 2);
+  }
+
+  create(){
+    this.grid = new Array(this.rows).fill(null).map(() => new Array(this.columns).fill(null));
+  }
+
+  add(table){
+    this.tables.push(table);
+  }
+
+  delete(table){
+    this.tables = this.tables.filter(e => e !== table);
   }
 
 }
--- src/app/modules/manager/room-editor/table-option/table-option.component.ts
+++ src/app/modules/manager/room-editor/table-option/table-option.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Table } from 'src/app/models/table';
+import { faCog } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-table-option',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableOptionComponent implements OnInit {
 
+  selected: boolean = false;
+  table: Table = {} as Table;
+
+  icon = faCog;
+
+  @Input() coordX: number;
+  @Input() coordY: number;
+
+  @Output() addTable: EventEmitter<Table> = new EventEmitter();
+  @Output() deleteTable: EventEmitter<Table> = new EventEmitter();
+  @Output() editTable: EventEmitter<Table> = new EventEmitter();
+
   constructor() { }
 
   ngOnInit(): void {
+    this.table = { x: this.coordX, y: this.coordY, status: 'free', number: null, seats: 0, tableID: 0, roomID: 0 };
+  }
+
+  select(){
+    if(this.selected){
+      if(confirm('Table will be deleted! Continue?')){
+        this.deleteTable.emit(this.table);
+        this.selected = !this.selected;
+      }
+    }
+    else{
+      this.addTable.emit(this.table);
+      this.selected = !this.selected;
+    }
+  }
+
+  edit(event){
+    event.stopPropagation();
+  }
+
+  getClass(){
+    if(this.selected) return "table-view table-option selected";
+    return "table-view table-option";
   }
 
 }
