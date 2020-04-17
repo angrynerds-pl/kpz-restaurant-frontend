e739cb5c0ed8f3e4e8501ef1a74ce866873aca16
KarolJaskolka <jaskolka.karol@gmail.com>
2020-04-17 20:43:25 +0200

    Added guards

--- src/app/modules/home/login/login.component.ts
+++ src/app/modules/home/login/login.component.ts
@@ -1,17 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
+import { Subscription } from 'rxjs';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
   loginForm: FormGroup;
 
-  constructor(private formBuilder:FormBuilder, private authService:AuthService) { }
+  loginSubscription: Subscription;
+
+  constructor(private formBuilder:FormBuilder, private authService:AuthService, private localStorageService:LocalStorageService) { }
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
@@ -23,7 +27,14 @@ export class LoginComponent implements OnInit {
   submit(event){
     event.preventDefault();
     console.log(this.loginForm.value);
-    // this.authService.login(this.loginForm.get('login').value, this.loginForm.get('password').value);
+    // this.loginSubscription = this.authService.login(this.loginForm.get('login').value, this.loginForm.get('password').value)
+    // .subscribe(data => {
+    //   this.localStorageService.setToken(data.token);
+    // })
+  }
+
+  ngOnDestroy(){
+    if(this.loginSubscription) this.loginSubscription.unsubscribe();
   }
 
 }
