d31bfcaa8c69d3d7cadb927f094bf74150b922c4
KarolJaskolka <jaskolka.karol@gmail.com>
2020-04-18 13:49:11 +0200

    Updated register

--- src/app/modules/home/login/login.component.ts
+++ src/app/modules/home/login/login.component.ts
@@ -15,7 +15,7 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
-      login: [null, Validators.required],
+      username: [null, Validators.required],
       password: [null, Validators.required]
     })
   }
--- src/app/modules/home/register/register.component.ts
+++ src/app/modules/home/register/register.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss', '../login/login.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  registerForm: FormGroup;
+
+  constructor(private formBuilder:FormBuilder, private router:Router) { }
 
   ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      username: [null, Validators.required],
+      password: [null, Validators.required],
+      firstName: [null, Validators.required],
+      lastName: [null, Validators.required],
+      restaurant: [null, Validators.required]
+    })
+  }
+
+  submit(event){
+    event.preventDefault();
+    console.log(this.registerForm.value);
+    // request
+    this.router.navigate(['/login']);
+  }
+
+  ngOnDestroy(){
+
   }
 
 }
