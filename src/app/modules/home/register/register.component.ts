5a667088d71141de7aad4401cf4243f47b047873
KarolJaskolka <jaskolka.karol@gmail.com>
2020-04-18 17:35:53 +0200

    Added pattern validation & link to register

--- src/app/modules/home/register/register.component.ts
+++ src/app/modules/home/register/register.component.ts
@@ -14,9 +14,12 @@ export class RegisterComponent implements OnInit, OnDestroy {
   constructor(private formBuilder:FormBuilder, private router:Router) { }
 
   ngOnInit(): void {
+
+    const pattern = '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{6,40}$';
+
     this.registerForm = this.formBuilder.group({
       username: [null, Validators.required],
-      password: [null, Validators.required],
+      password: [null, [Validators.required, Validators.pattern(pattern)]],
       firstName: [null, Validators.required],
       lastName: [null, Validators.required],
       restaurant: [null, Validators.required]
