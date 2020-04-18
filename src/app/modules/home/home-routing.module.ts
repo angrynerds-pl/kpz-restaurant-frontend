c59281ba72615131e537d62161b0b66765ff24e3
KarolJaskolka <jaskolka.karol@gmail.com>
2020-04-18 13:05:23 +0200

    Added register component

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeMainComponent } from './home-main/home-main.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full'},
-  { path: 'login', component: HomeMainComponent}
+  { 
+    path: '', 
+    component: HomeMainComponent,
+    children: [
+      { 
+        path: '', 
+        redirectTo: 'login',
+        pathMatch: 'full'
+      },
+      { 
+        path: 'login', 
+        component: LoginComponent
+      },
+      { 
+        path: 'register', 
+        component: RegisterComponent
+      }
+    ]
+  }
 ];
 
 @NgModule({
