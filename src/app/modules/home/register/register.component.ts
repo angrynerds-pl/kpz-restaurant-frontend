e9e1caf3ee3ec8ae83cd50abc93c5df9f4259a12
KarolJaskolka <jaskolka.karol@gmail.com>
2020-04-22 15:15:40 +0200

    Added http request to register

--- src/app/modules/home/register/register.component.ts
+++ src/app/modules/home/register/register.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { Subscription } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -10,31 +13,36 @@ import { Router } from '@angular/router';
 export class RegisterComponent implements OnInit, OnDestroy {
 
   registerForm: FormGroup;
+  authSubscription: Subscription;
 
-  constructor(private formBuilder:FormBuilder, private router:Router) { }
+  constructor(private formBuilder:FormBuilder, private router:Router, 
+    private authSerivce:AuthService, private toastrSerivce:ToastrService) { }
 
   ngOnInit(): void {
 
     const pattern = '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{6,40}$';
 
     this.registerForm = this.formBuilder.group({
-      username: [null, Validators.required],
-      password: [null, [Validators.required, Validators.pattern(pattern)]],
-      firstName: [null, Validators.required],
-      lastName: [null, Validators.required],
-      restaurant: [null, Validators.required]
+      Username: [null, Validators.required],
+      Password: [null, [Validators.required, Validators.pattern(pattern)]],
+      FirstName: [null, Validators.required],
+      LastName: [null, Validators.required],
+      RestaurantName: [null, Validators.required]
     })
   }
 
   submit(event){
     event.preventDefault();
-    console.log(this.registerForm.value);
-    // request
-    this.router.navigate(['/login']);
+    this.authSubscription = this.authSerivce.register(this.registerForm.value).subscribe(data => {
+      this.toastrSerivce.success('Great!', 'Your account has been created!');
+      this.router.navigate(['/login']);
+    }, error => {
+      this.toastrSerivce.error('Error!', 'Something went wrong :/');
+    })
   }
 
   ngOnDestroy(){
-
+    if(this.authSubscription) this.authSubscription.unsubscribe();
   }
 
 }
