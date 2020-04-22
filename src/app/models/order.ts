876c393cb23b0d295f3c2de8a1508b860b35fd5d
Ecuros <36072074+Ecuros@users.noreply.github.com>
2020-04-22 14:25:14 +0200

    added spaces

--- src/app/models/order.ts
+++ src/app/models/order.ts
@@ -3,7 +3,7 @@ import { ProductsInOrder } from './products-in-order';
 export interface Order {
     id:number;
     tableId: number;
-    date:string;
-    status:boolean; 
-    productsInOrder:Array<ProductsInOrder>;  
+    date: string;
+    status: boolean; 
+    productsInOrder: Array<ProductsInOrder>;  
 }
